878a08c0e27da31931d9c216e1462e180cc5e1ca
jtk0002 <2016HellcatSRT?>
2020-02-24 20:50:45 -0500

    Finished lifestyle buttons and screens

--- app/src/main/java/com/example/myhealth/LifestyleFragment.java
+++ app/src/main/java/com/example/myhealth/LifestyleFragment.java
@@ -27,7 +27,7 @@ import com.example.myhealth.ui.lifestyle.WeightLogActivity;
  * Activities that contain this fragment must implement the
  * {@link LifestyleFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link LifestyleFragment#newInstance} factory method to
+ * Use the {@link LifestyleFragment} factory method to
  * create an instance of this fragment.
  */
 public class LifestyleFragment extends Fragment {
@@ -40,6 +40,7 @@ public class LifestyleFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+
     private OnFragmentInteractionListener mListener;
 
     public LifestyleFragment() {
@@ -50,9 +51,6 @@ public class LifestyleFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LifestyleFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static LifestyleFragment nutrientLog(String fluid, double volume, String food, double calories) {
